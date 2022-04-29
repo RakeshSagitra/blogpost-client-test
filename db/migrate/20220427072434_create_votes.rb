8a5a7f3c24a7fae539025bb1256c4fb17e53d92f
Rakesh Sagitra <rakeshsagitra9@gmail.com>
2022-04-29 15:10:37 +0530

    Added a comment in migration

--- db/migrate/20220427072434_create_votes.rb
+++ db/migrate/20220427072434_create_votes.rb
@@ -2,6 +2,8 @@ class CreateVotes < ActiveRecord::Migration[6.1]
   def change
     create_table :votes do |t|
       t.belongs_to :user, null: false, foreign_key: true
+      # created this as polymorphic association so that it can be used as in various places
+      # i.e. upvoting in comments and post
       t.references :votable, polymorphic: true, null: false
 
       t.timestamps
